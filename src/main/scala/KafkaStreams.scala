66cd62e5d722212a9a67735233588eaf7ea814dd
Duy Hoang <duyhoang123@hotmail.co.uk>
2024-01-09 13:47:57 +0000

    forgot the reason for committing

--- src/main/scala/KafkaStreams.scala
+++ src/main/scala/KafkaStreams.scala
@@ -1,12 +1,20 @@
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.streams.scala.serialization.Serdes
+
 object KafkaStreams {
 
   // Daniel uses this method as opposed to data in case classes for some reason
   object Domain {
     // this is the Domain outline for an online store
-    type UserId = String
-    type Profile = String
-    type Product = String
-    type OrderId = String
+    // The IDE were telling you to ensure that the below we privately accessed
+    private type UserId = String
+    private type Profile = String
+    private type Product = String
+    private type OrderId = String
 
     case class Order(orderId: OrderId, userId: UserId, products: List[Product], amount: Double)
     case class Discount(profile: Profile, amount: Double)
@@ -16,13 +24,33 @@ object KafkaStreams {
   object Topics {
     val OrdersByUser = "orders-by-user"
     val DiscountProfileByUser = "discount-profiles-by-user"
-    val Discounts = "disocunts"
+    val Discounts = "discounts"
     val Orders = "orders"
     val PaidOrders = "paid-orders"
   }
 
+  /*
+    Three primary entities within Kafka (parts of a stream processor)
+      1. source = emits elements
+      2. flow = transforms elements along the way (e.g. map)
+      3. sink = "ingests' elements
+   */
+  import Domain._
+  // Note that below we are using the inbuilt 'Serde' in Kafka wherein we have to explicitly state both a Serializer and Deserializer
+  // Remember that with a deserializer, you have to alter an object into bytes -> the function was altered from an implicit val towards an implicit def also and this needs to be understood
+  implicit def serdeOrder[A : Decoder : Encoder ]: Serde[Order] = {
+    val serializer = (order: Order) => order.asJson.noSpaces.getBytes()
+    val deserializer = (bytes: Array[Byte]) => {
+      val string = new String(bytes)
+      decode[Order](string).toOption
+    }
+
+    Serdes.fromFn[Order](serializer, deserializer)
+  }
+
+
   def main(args: Array[String]): Unit = {
-    // Rather than manually writing out the Kafka command for each of the topics, the below is a better option
+    // The below will be printed for the topics such that there is a broker in the bash
     List(
       "orders-by-user" ,
       "discount-profiles-by-user" ,
@@ -31,7 +59,7 @@ object KafkaStreams {
       "payments" ,
       "paid-orders" ,
     ).foreach { topic =>
-      println("")
+      println(s"kafka-topics -- bootstrap-server localhost:9092 --topic ${topic} --create")
     }
   }
 
