c73913597e586d3a8b769cb81a8f84f20c5b149a
Duy Hoang <duyhoang123@hotmail.co.uk>
2024-01-14 12:01:25 +0000

    completed all the exercises

--- src/main/scala/KafkaStreams.scala
+++ src/main/scala/KafkaStreams.scala
@@ -3,10 +3,16 @@ import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
 import org.apache.kafka.common.serialization.Serde
-import org.apache.kafka.streams.kstream.{GlobalKTable, KStream, KTable}
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+import org.apache.kafka.streams.kstream.{GlobalKTable, JoinWindows}
+import org.apache.kafka.streams.scala.kstream.{KStream, KTable}
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.serialization.Serdes
 import org.apache.kafka.streams.scala.ImplicitConversions._
+
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+import java.util.Properties
 
 object KafkaStreams {
 
@@ -19,9 +25,9 @@ object KafkaStreams {
     type Product = String
     type OrderId = String
 
-    case class Order(orderId: OrderId, userId: UserId, products: List[Product], amount: Double)
-    case class Discount(profile: Profile, amount: Double)
-    case class Payment(orderId: OrderId, status: String)
+    final case class Order(orderId: OrderId, userId: UserId, products: List[Product], amount: Double)
+    final case class Discount(profile: Profile, amount: Double)
+    final case class Payment(orderId: OrderId, status: String)
   }
 
   object Topics {
@@ -54,33 +60,79 @@ object KafkaStreams {
     Serdes.fromFn[A](serializer, deserializer)
   }
 
-  // topology
-  private val builder = new StreamsBuilder()
+  def main(args: Array[UserId]): Unit = {
 
-  // KStream
-  val usersOrdersStream: KStream[UserId, Order] = builder.stream[UserId, Order](OrdersByUser)
+    // topology
+    val builder = new StreamsBuilder()
 
-  // KTable - is distributed
-  val usersProfilesTable: KTable[UserId, Profile] = builder.table[UserId, Profile](DiscountProfileByUser)
+    // KStream
+    val usersOrdersStream: KStream[UserId, Order] = builder.stream[UserId, Order](OrdersByUser)
 
-  // GlobalKTable - copied to all the nodes
-  val discountProfilesGTable: GlobalKTable[Profile, Discount] = builder.globalTable[Profile, Discount](Discounts)
+    // KTable - is distributed
+    val usersProfilesTable: KTable[UserId, Profile] = builder.table[UserId, Profile](DiscountProfileByUser)
 
-  builder.build()
+    // GlobalKTable - copied to all the nodes
+    val discountProfilesGTable: GlobalKTable[Profile, Discount] = builder.globalTable[Profile, Discount](Discounts)
 
-
-  def main(args: Array[String]): Unit = {
-    // The below will be printed for the topics such that there is a broker in the bash
-    List(
-      "orders-by-user" ,
-      "discount-profiles-by-user" ,
-      "discounts" ,
-      "orders" ,
-      "payments" ,
-      "paid-orders" ,
-    ).foreach { topic =>
-      println(s"kafka-topics -- bootstrap-server localhost:9092 --topic ${topic} --create")
+    // KStream transformation: filter, map, mapValues, flatMap, flatMapValues
+    val expensiveOrders = usersOrdersStream.filter { (userId, order) =>
+      order.amount > 1000
     }
+
+    val listOfProducts = usersOrdersStream.mapValues { order =>
+      order.products
+    }
+
+    val productsStream = usersOrdersStream.flatMapValues(_.products)
+
+    // join stream and table
+    val ordersWithUserProfiles = usersOrdersStream.join(usersProfilesTable) { (order, profile) =>
+      (order, profile)
+    }
+
+    val discountedOrdersStream = ordersWithUserProfiles.join(discountProfilesGTable)(
+      { case (userId, (order, profile)) => profile }, // key of the join - picked from the "left" stream
+      { case ((order, profile), discount) => order.copy(amount = order.amount - discount) } // values of the matched records
+    )
+
+    // pick another identifier
+    val ordersStream = discountedOrdersStream.selectKey((userId, order) => order.orderId)
+    val paymentsStream = builder.stream[OrderId, Payment](Payments)
+
+    val joinWindow = JoinWindows.of(Duration.of(5, ChronoUnit.MINUTES))
+    val joinOrdersPayments = (order: Order, payment: Payment) => if (payment.status == "PAID") Option(order) else Option.empty[Order]
+
+    val ordersPaid = ordersStream.join(paymentsStream)(joinOrdersPayments, joinWindow)
+      .flatMapValues(maybeOrder => maybeOrder.toIterable)
+
+    // sink
+    ordersPaid.to(PaidOrders)
+
+    val topology = builder.build()
+
+    val props = new Properties
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "orders-application")
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.stringSerde.getClass)
+
+    val application = new KafkaStreams(topology, props)
+    application.start()
+//    println(topology.describe())
+
   }
+
+//  def main(args: Array[String]): Unit = {
+//    // The below will be printed for the topics such that there is a broker in the bash
+////    List(
+////      "orders-by-user" ,
+////      "discount-profiles-by-user" ,
+////      "discounts" ,
+////      "orders" ,
+////      "payments" ,
+////      "paid-orders" ,
+////    ).foreach { topic =>
+////      println(s"kafka-topics -- bootstrap-server localhost:9092 --topic ${topic} --create")
+////    }
+//  }
 
 }
